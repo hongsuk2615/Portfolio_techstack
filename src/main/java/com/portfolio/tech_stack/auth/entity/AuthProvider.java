128ed36de903dab573290e9666d42f664abfa408
김홍석 <hongsuk2615@gmail.com>
2026-02-04 12:54:02 +0900

    feat: 유저권한, 인증 제공처 ENUM 추가

---
+++ src/main/java/com/portfolio/tech_stack/auth/entity/AuthProvider.java
@@ -0,0 +1,7 @@
+package com.portfolio.tech_stack.auth.entity;
+
+public enum AuthProvider {
+    DEFAULT,   // 기본 회원가입
+    GOOGLE,
+    GITHUB
+}
---
+++ src/main/java/com/portfolio/tech_stack/auth/entity/UserRole.java
@@ -0,0 +1,6 @@
+package com.portfolio.tech_stack.auth.entity;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
