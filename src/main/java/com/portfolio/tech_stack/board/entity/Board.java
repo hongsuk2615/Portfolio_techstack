5db6438f524796c01658a37add6e92d4bf0bff72
김홍석 <hongsuk2615@gmail.com>
2026-02-08 05:06:55 +0900

    feat : 게시판 entity

---
+++ src/main/java/com/portfolio/tech_stack/board/entity/Board.java
@@ -0,0 +1,51 @@
+package com.portfolio.tech_stack.board.entity;
+
+import com.portfolio.tech_stack.auth.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "boards",
+    indexes = {
+        @Index(name = "idx_user_id", columnList = "userId")
+    })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = true)
+    private LocalDateTime deletedAt;
+
+
+
+
+}
